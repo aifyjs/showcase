b861f730826d9928af9e4b02ae670e4255840c07
xprilion <xprilion@gmail.com>
2020-02-27 02:25:11 +0530

    set

---
+++ main.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+	var ai = new aifyjs();
+
+	ai.labels();
+
+	ai.captions();
+
+	// window.setTimeout(function(){ai.displayCaptions();}, 5000);
+	// window.setTimeout(function(){ai.displayLabels();}, 5000);
+
+});
